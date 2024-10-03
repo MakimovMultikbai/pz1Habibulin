e1a17bab0bcc480ba8cda7dc2f7b4a330c0d0c57
Aldiar <makimovaldiarsamatovich@gmail.com>
2024-10-03 12:51:59 +0500

    Добавьте файлы проекта.

---
+++ pz1Habibulin/Class1.cs
@@ -0,0 +1,37 @@
+﻿using pz1Habibulin;
+using System;
+using static pz1Habibulin.mathematic;
+
+namespace pz1Habibulin
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            mathematic math = new mathematic();
+
+            double a = 10.2;
+            double b = 8.7;
+         
+            Console.WriteLine(math.Plus(a, b));
+            Console.WriteLine();
+
+            Console.WriteLine(math.Minus(a, b));
+            Console.WriteLine();
+
+            Console.WriteLine(math.Multiply(a, b));
+            Console.WriteLine();
+
+            Console.WriteLine(math.Divide(a, b));
+            Console.WriteLine();
+
+            Console.WriteLine(math.Pow(a, (int)b));
+            Console.WriteLine();
+
+            Console.WriteLine(math.bolshee(a,b));
+            Console.WriteLine(); 
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pz1Habibulin/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz1Habibulin
+{
+    public class mathematic
+    {
+        public string Plus(double a, double b)
+        {
+            return $"{a} + {b} = {a+b}";
+        }
+
+        public string Minus(double a, double b)
+        {
+            return $"{a} - {b} = {a - b}";
+        }
+
+        public string Divide(double a, double b)
+        {
+            return $"{a} / {b} = {a / b}";
+        }
+
+        public string Multiply(double a, double b)
+        {
+            return $"{a} * {b} = {a * b}";
+        }
+
+        public string Pow(double a, int b)
+        {
+            double x = a;
+            for (int i = 0; i < b; i++) 
+            {
+                x *= a;      
+            }
+            return $"{a}^{b} = {x}";
+        }
+
+        public string bolshee(double a, double b)
+        {
+            if (a < b) 
+            {
+                return $"Большее число {b}";
+            }
+            else if (a > b)
+            {
+                return $"Большее число {a}";
+            }
+            else
+            {
+                return $"Числа равны";
+            }
+        }
+    }
+}
